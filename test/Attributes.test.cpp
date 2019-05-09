520c06542a34b272cbfc155324dd71ab87327de0
Laurin Agostini <thevolut@gmail.com>
2019-05-09 21:25:13 +0200

    Added more tests

--- test/Attributes.test.cpp
+++ test/Attributes.test.cpp
@@ -3,19 +3,51 @@
 #include "test/Types/Person.hpp"
 
 SCENARIO("attributes", "[attribute]") {
-	GIVEN("a variable and an object of type 'Person'") {
+	GIVEN("a nil variable and an object of type 'Person'") {
 		Person p_var;
 		Person p_obj = ot::empty();
 		WHEN("attribute 'name' was not set") {
 			THEN("reading 'name' should return empty string") {
 				REQUIRE(p_var[name] == std::string());
+				REQUIRE(name(p_var) == std::string());
 				REQUIRE(p_obj[name] == std::string());
+				REQUIRE(name(p_obj) == std::string());
 			}
 		}
 		WHEN("attribute 'age' was not set") {
 			THEN("reading 'age' should return 0") {
 				REQUIRE(p_var[age] == 0);
 				REQUIRE(p_obj[age] == 0);
+			}
+		}
+		WHEN("attribute 'name' was set on object") {
+			p_obj(name, std::string("Peter"));
+			THEN("reading 'name' from object should return 'Peter'") {
+				REQUIRE(p_obj[name] == "Peter");
+			}
+		}
+		WHEN("attribute 'name' was set on nil variable") {
+			p_var(name, std::string("Hans"));
+			THEN("reading 'name' from variable should return empty string") {
+				REQUIRE(p_var[name] == std::string());
+			}
+		}
+	}
+	GIVEN("an object of type 'Person' and a variable referencing it") {
+		Person p_obj = ot::empty();
+		Person p_var = p_obj;
+		WHEN("attribute 'name' was set on object") {
+			p_obj(name, std::string("Peter"));
+			THEN("reading 'name' from object or variable should return 'Peter'") {
+				REQUIRE(p_obj[name] == "Peter");
+				REQUIRE(p_var[name] == "Peter");
+			}
+		}
+		WHEN("attribute 'name' was set on variable") {
+			p_var(name, std::string("Hans"));
+			THEN("reading 'name' from object or variable should return 'Hans'") {
+				REQUIRE(p_obj[name] == "Hans");
+				REQUIRE(p_var[name] == "Hans");
 			}
 		}
 	}
--- test/Types.test.cpp
+++ test/Types.test.cpp
@@ -3,17 +3,17 @@
 #include "test/Types/Person.hpp"
 
 SCENARIO("type and object creation", "[type, object]") {
-    GIVEN("a type 'Person'") {
+    GIVEN("a type 'Person' and a type 'Skill'") {
         WHEN("a type variable is default created") {
             Person p;
-            THEN("p is nil") {
+            THEN("is p nil") {
                 REQUIRE(p == ot::nil);
                 REQUIRE(!p);
 			}
 		}
         WHEN("a type variable is created with nil") {
             Person p = ot::nil;
-            THEN("p is nil") {
+            THEN("is p nil") {
                 REQUIRE(p == ot::nil);
                 REQUIRE(!p);
             }
@@ -21,20 +21,20 @@ SCENARIO("type and object creation", "[type, object]") {
         WHEN("two type variables are created with nil") {
             Person p1 = ot::nil;
             Person p2 = ot::nil;
-            THEN("p1 and p2 are equal") {
+            THEN("are p1 and p2 equal") {
                 REQUIRE(p1 == p2);
             }
         }
         WHEN("a type object is created with empty()") {
             Person p = ot::empty();
-            THEN("p is not nil") {
+            THEN("is p not nil") {
                 REQUIRE(p != ot::nil);
                 REQUIRE(p);
             }
         }
 		WHEN("a type object is created with constructor") {
 			Person p(true);
-			THEN("p is not nil") {
+			THEN("is p not nil") {
 				REQUIRE(p != ot::nil);
 				REQUIRE(p);
 			}
@@ -42,22 +42,29 @@ SCENARIO("type and object creation", "[type, object]") {
         WHEN("a type object and a nil type variable are created") {
             Person p1 = ot::empty();
             Person p2 = ot::nil;
-            THEN("p1 is not equal to p2") {
+            THEN("is p1 not equal to p2") {
                 REQUIRE(p1 != p2);
             }
         }
         WHEN("two type objects are created") {
             Person p1 = ot::empty();
             Person p2 = ot::empty();
-            THEN("p1 is not equal to p2") {
+            THEN("is p1 not equal to p2") {
                 REQUIRE((p1 != p2));
             }
         }
 		WHEN("two type objects of different types are created") {
 			Person p = ot::empty();
 			Skill s = ot::empty();
-			THEN("p is not equal to s") {
+			THEN("is p not equal to s") {
 				REQUIRE((p != s));
+			}
+		}
+		WHEN("an object and a reference to that object are created") {
+			Person p1 = ot::empty();
+			Person p2 = p1;
+			THEN("is p1 equal to p2") {
+				REQUIRE((p1 == p2));
 			}
 		}
 	}
